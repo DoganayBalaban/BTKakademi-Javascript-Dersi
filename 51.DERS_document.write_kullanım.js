686a28b7e85dafe436480248190827befe452adc
Doğanay Balaban <dbalaban1907@gmail.com>
2023-02-23 14:36:42 +0300

    57.ders sonu

---
+++ 51.DERS_document.write_kullanım.js
@@ -0,0 +1,4 @@
+//document.write    =  Dökukmana çıktı sağlar (nesnesel - DOM)
+//document.writeln  =  Yeni satır başı yapar.(nesnesel - DOM)
+document.writeln("Merhaba")
+document.write("Javascript")
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var isim    = "Doğanay"
 var soyisim = "Balaban"
 var yaş     =  19
-alert(yaş)
+alert(yaş)  //Sayfa içerisinde uyarı penceresi açar
+            //window.alert() sayfa içerisinde uyarı penceresi açar.(nesnesel - BOM)
 
 
